1e96985b595bf74d39ec7bba9baecd0090ccd0f2
tstuyver <thijsstuyver@hotmail.com>
2022-02-16 15:51:49 -0500

    Remove pre-defined descriptor names and add .gitignore

--- process_desc/post_process.py
+++ process_desc/post_process.py
@@ -6,8 +6,8 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 tqdm.pandas()
 
-REACTION_DESCRIPTORS = ["G", "DE_RP", "G_alt1", "G_alt2"]
-GLOBAL_SCALE = ['partial_charge', 'fukui_neu', 'fukui_elec','parr_elec','parr_neu','spin_dens','spin_dens_triplet']
+#REACTION_DESCRIPTORS = ["G", "DE_RP", "G_alt1", "G_alt2"]
+#GLOBAL_SCALE = ['partial_charge', 'fukui_neu', 'fukui_elec','parr_elec','parr_neu','spin_dens','spin_dens_triplet']
 ATOM_SCALE = ['NMR']
 
 
@@ -39,6 +39,9 @@ def modify_scaled_df(df, scalers):
 
     return df
 
+def min_max_by_atom(atoms, data, scaler):
+    data = [scaler[a].transform(np.array([[d]]))[0][0] for a, d in zip(atoms, data)]
+    return np.array(data)
 
 def min_max_normalize(df, scalers=None, train_smiles=None):
     if train_smiles is not None:
@@ -49,26 +52,23 @@ def min_max_normalize(df, scalers=None, train_smiles=None):
     if scalers is None:
         scalers = get_scaler(ref_df)
 
-    for column in GLOBAL_SCALE:
-        scaler = scalers[column]
-        df[column] = df[column].apply(lambda x: scaler.transform(x.reshape(-1, 1)).reshape(-1))
-
-    def min_max_by_atom(atoms, data, scaler):
-        data = [scaler[a].transform(np.array([[d]]))[0][0] for a, d in zip(atoms, data)]
-        return np.array(data)
-
     if ATOM_SCALE:
         print('postprocessing atom-wise scaling')
         df['atoms'] = df.smiles.apply(lambda x: get_atoms(x))
-        for column in ATOM_SCALE:
+
+    for column in df.columns:
+        if column not in ATOM_SCALE and column not in ['smiles', 'atoms', 'bond_order', 'bond_length']:
+            scaler = scalers[column]
+            df[column] = df[column].apply(lambda x: scaler.transform(x.reshape(-1, 1)).reshape(-1))
+
+        elif column in ATOM_SCALE:
             df[column] = df.progress_apply(lambda x: min_max_by_atom(x['atoms'], x[column], scalers[column]), axis=1)
 
-    df['bond_order_matrix'] = df.apply(lambda x: bond_to_matrix(x['smiles'], x['bond_order']), axis=1)
-    df['distance_matrix'] = df.apply(lambda x: bond_to_matrix(x['smiles'], x['bond_length']), axis=1)
+        elif column in ['bond_order', 'bond_length']:
+            df[f'{column}_matrix'] = df.apply(lambda x: bond_to_matrix(x['smiles'], x['bond_order']), axis=1)
 
     df = modify_scaled_df(df, scalers)
-    df = df[['smiles', 'partial_charge', 'fukui_neu', 'fukui_elec', 'NMR', 'parr_elec', 'parr_neu', 'spin_dens',
-             'spin_dens_triplet', 'bond_order_matrix', 'distance_matrix']]
+    df = df[[column for column in df.columns if column not in ['atoms', 'bond_order', 'bond_length']]]
     df = df.set_index('smiles')
 
     return df, scalers
@@ -76,17 +76,20 @@ def min_max_normalize(df, scalers=None, train_smiles=None):
 
 def get_scaler(df):
     scalers = {}
-    for column in GLOBAL_SCALE:
-        scaler = MinMaxScaler()
-        data = np.concatenate(df[column].tolist()).reshape(-1, 1)
-
-        scaler.fit(data)
-        scalers[column] = scaler
 
     if ATOM_SCALE:
         atoms = df.smiles.apply(lambda x: get_atoms(x))
         atoms = np.concatenate(atoms.tolist())
-        for column in ATOM_SCALE:
+
+    for column in df.columns:
+        if column not in ATOM_SCALE and column != 'smiles':
+            scaler = MinMaxScaler()
+            data = np.concatenate(df[column].tolist()).reshape(-1, 1)
+
+            scaler.fit(data)
+            scalers[column] = scaler
+
+        elif column in ATOM_SCALE:
             data = np.concatenate(df[column].tolist())
 
             data = pd.DataFrame({'atoms': atoms, 'data': data})
@@ -140,15 +143,17 @@ def min_max_normalize_reaction(df, scalers=None, train_smiles=None):
 
     if scalers is None:
         scalers = {}
-        for column in REACTION_DESCRIPTORS:
-            scaler = StandardScaler()
-            data = ref_df[column].values.reshape(-1, 1).tolist()
+        for column in df.columns:
+            if column != 'smiles':
+                scaler = StandardScaler()
+                data = ref_df[column].values.reshape(-1, 1).tolist()
 
-            scaler.fit(data)
-            scalers[column] = scaler
+                scaler.fit(data)
+                scalers[column] = scaler
 
-    for column in REACTION_DESCRIPTORS:
-        scaler = scalers[column]
-        df[column] = df[column].apply(lambda x: scaler.transform([[x]])[0])
+    for column in df.columns:
+        if column != 'smiles':
+            scaler = scalers[column]
+            df[column] = df[column].apply(lambda x: scaler.transform([[x]])[0])
 
     return df, scalers
