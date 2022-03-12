9f4e49327276543034b224e86df25ffaa4baa984
tstuyver <thijsstuyver@hotmail.com>
2022-03-11 19:14:04 -0500

    Clean up process descs folder

---
+++ process_descs/__init__.py
@@ -0,0 +1,2 @@
+from .post_process import min_max_normalize_atom_descs, min_max_normalize_reaction_descs
+from .predict_descs import predict_atom_descs, predict_reaction_descs, reaction_to_reactants
